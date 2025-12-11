b6164f996e7aa0323959974fa4207112cf75bf2a
El-sheshtawy <el-sheshtawy@example.com>
2025-12-12 01:52:45 +0200

    quick fix

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -39,25 +39,21 @@ if (!empty($_COOKIE['lang']) and $_COOKIE['lang'] == 2) {
                 <div class="slider__nav-item swiper-slide orange-bg" data-background="assets/img/slider/nav/slider-nav-4.jpg">
                     <div class="slider__nav-content">
                         <span>{{trans('web.users')}}</span>
-                        <h4 style="display: none;">{{\App\User::where('active', 'active')->count()}}</h4>
                     </div>
                 </div>
                 <div class="slider__nav-item swiper-slide blue-bg" data-background="assets/img/slider/nav/slider-nav-1.jpg">
                     <div class="slider__nav-content">
                         <a href="{{url('posts')}}" style="display: inline-block; font-size: 15px; color: #ffffff; opacity: 0.8;">{{trans('web.news')}}</a>
-                        <h4 style="display: none;">{{\App\Post::where('active', 'active')->count()}}</h4>
                     </div>
                 </div>
                 <div class="slider__nav-item swiper-slide pink-bg" data-background="assets/img/slider/nav/slider-nav-2.jpg">
                     <div class="slider__nav-content">
                         <a href="{{url('gallery')}}" style="display: inline-block; font-size: 15px; color: #ffffff; opacity: 0.8;">{{trans('web.images')}}</a>
-                        <h4 style="display: none;">{{\App\Gallery::where('active', 'active')->count()}}</h4>
                     </div>
                 </div>
                 <div class="slider__nav-item swiper-slide green-bg" data-background="assets/img/slider/nav/slider-nav-3.jpg">
                     <div class="slider__nav-content">
                         <a href="{{url('locations')}}" style="display: inline-block; font-size: 15px; color: #ffffff; opacity: 0.8;">{{trans('web.locations')}}</a>
-                        <h4 style="display: none;">{{\App\Location::where('active', 'active')->count()}}</h4>
                     </div>
                 </div>
             </div>
