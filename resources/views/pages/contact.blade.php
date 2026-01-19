82ee2d38360423f2142abb21ad98b7ab7b52ab9d
El-sheshtawy <el-sheshtawy@example.com>
2026-01-19 22:30:14 +0200

    new

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -48,9 +48,9 @@ if (!empty($_COOKIE['lang']) and $_COOKIE['lang'] == 2) {
                                 </ol>
                             </nav>
                         </div>
-                        <div class="d-flex align-items-center">
+                        <div class="d-flex align-items-center" style="gap: 30px;">
                             <h5 class="page__title-3 mb-0">{{trans('web.contact')}}</h5>
-                            <img src="{{url('website/public/images/QRCode.jpg')}}" alt="QR Code" style="max-width: 100px; margin-left: 30px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
+                            <img src="{{url('website/public/images/QRCode.jpg')}}" alt="QR Code" style="max-width: 100px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0,0,0,0.1);">
                         </div>
                     </div>
                 </div>
